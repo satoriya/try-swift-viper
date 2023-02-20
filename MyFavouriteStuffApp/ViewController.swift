cde279128153e1e6d1c5c00b8558083883b22af4
satriya <satriya.wicaksana@phincon.com>
2023-02-20 10:50:20 +0700

    Initial Commit

---
+++ MyFavouriteStuffApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavouriteStuffApp
+//
+//  Created by Phincon on 20/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

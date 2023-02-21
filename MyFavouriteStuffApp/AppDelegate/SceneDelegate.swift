48929b93de7d74fdb1d29cfa6336a77a17a1103f
satoriya <satriya.wicaksana89@gmail.com>
2023-02-21 16:21:17 +0700

    Add builder to scene

--- MyFavouriteStuffApp/AppDelegate/SceneDelegate.swift
+++ MyFavouriteStuffApp/AppDelegate/SceneDelegate.swift
@@ -18,8 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = MyListViewController()
-        let navigationController = UINavigationController(rootViewController: vc)
+        let navigationController = UINavigationController(rootViewController: MyListFactory.build())
         
         window.rootViewController = navigationController
         self.window = window

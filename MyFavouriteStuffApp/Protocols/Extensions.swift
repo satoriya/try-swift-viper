31dce4fd6186f748c818d1ec6f37d6e45555f8ae
satoriya <satriya.wicaksana89@gmail.com>
2023-02-21 16:24:35 +0700

    Update Extensions.swift
    - card view
    - add date generator to interactor

--- MyFavouriteStuffApp/Protocols/Extensions.swift
+++ MyFavouriteStuffApp/Protocols/Extensions.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    
+    func cardView(cornerRadius: CGFloat, opacity: Float, offset: CGSize) {
+        let layers = self.layer
+        layers.cornerRadius = cornerRadius
+        layers.shadowOpacity = opacity
+        layers.shadowOffset = offset
+    }
+}
+
+extension MyListInteractor {
+    func generateDate(custom: Bool = false, time: Double = 0) -> String {
+        var currentDateTime = Date()
+        if custom {
+            currentDateTime = Date(timeIntervalSince1970: (time / 1000))
+        }
+        
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        
+        let dates = formatter.string(from: currentDateTime)
+        return dates
+    }
+}
